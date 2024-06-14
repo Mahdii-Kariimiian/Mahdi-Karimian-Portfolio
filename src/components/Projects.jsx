cad56a019107125929e7c7407c4f6d33d456ab76
Mahdi Karimian <mahdii.kariimiian@gmail.com>
2024-06-14 17:45:18 +0200

    edit padding

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,29 +10,25 @@ function Projects({ href, name, src, githubrepo, techs }) {
           ))
         : null;
     return (
-        <div className="max-w-[600px] relative font-raleway transition ring-4 ring-white flex flex-col items-center text-center min-w-[250px] outline-2 outline-transparent rounded-md mx-5">
-            <div>
-                <img
-                    className="max-w-full rounded-md p-2"
-                    src={`../../img/screenshots/${src}`}
-                    alt={name}
-                />
-                <div className="xl:absolute max-md:absolute max-sm:static lg:font-bold flex gap-2 flex-wrap right-6 top-6">
-                    {usedTechnologies}
-                </div>
-                <div className="flex sm:flex-row gap-8 md:justify-around items-center flex-wrap">
-                    <a className="p-3 flex flex-col gap-6 mb-3" href={href}>
-                        <h3 className="hover:outline-amber-700 transition text-lg whitespace-nowrap outline outline-white p-2 rounded-md max-w-[max-content]">
-                            {name}
-                        </h3>
+        <div className="max-w-[600px] relative font-raleway transition ring-4 ring-white flex flex-col items-center space-y-4 text-center pb-5 min-w-[250px] outline-2 outline-transparent rounded-md mx-5">
+            <img
+                className="max-w-full rounded-md p-2"
+                src={`../../img/screenshots/${src}`}
+                alt={name}
+            />
+            <div className="xl:absolute max-md:absolute max-sm:static lg:font-bold flex gap-2 flex-wrap right-6 top-6">
+                {usedTechnologies}
+            </div>
+            <div className="flex sm:flex-row gap-8 md:justify-around items-start flex-wrap">
+                <h3 className="hover:outline-amber-700 transition text-lg whitespace-nowrap outline outline-white p-2 rounded-md max-w-[max-content]">
+                    <a href={href}>{name}</a>
+                </h3>
+                <div className="cursor-pointer">
+                    <a href={githubrepo}>
+                        <button className="hover:outline-amber-700 transition text-lg whitespace-nowrap outline outline-white p-2 rounded-md">
+                            Github Repo
+                        </button>
                     </a>
-                    <div className="cursor-pointer">
-                        <a href={githubrepo}>
-                            <button className="hover:outline-amber-700 transition text-lg whitespace-nowrap outline outline-white p-2 rounded-md">
-                                Github Repo
-                            </button>
-                        </a>
-                    </div>
                 </div>
             </div>
         </div>
