fb49fc75ae4faa4eb56526615225dc8e9c536a8f
Mahdi Karimian <mahdii.kariimiian@gmail.com>
2024-06-17 14:35:29 +0200

    edit projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,7 +19,7 @@ function Projects({ href, name, src, githubrepo, techs }) {
             <div className="xl:absolute max-md:absolute max-sm:static lg:font-bold flex gap-2 flex-wrap right-6 top-6">
                 {usedTechnologies}
             </div>
-            <div className="flex sm:flex-row gap-8 md:justify-around items-start flex-wrap">
+            <div className="flex sm:flex-row gap-8 sm:justify-around flex-wrap p-2 justify-center">
                 <h3 className="hover:outline-amber-700 transition text-lg whitespace-nowrap outline outline-white p-2 rounded-md max-w-[max-content]">
                     <a href={href}>{name}</a>
                 </h3>
