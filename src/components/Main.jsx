d1bbfd7f461971e21dd91372b738ee9b7d67bf14
Mahdi Karimian <mahdii.kariimiian@gmail.com>
2024-11-10 11:32:52 +0100

    update about me

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,30 @@
 function Main() {
     return (
-        <div >
+        <div>
             <p className="hover:scale-105 transition-transform mb-5">
-                My life has always been associated with creating any kind of
-                contents. I have experienced various careers such as writing,
-                journalism, content creation and podcasting, and I achieved
-                great success in all these jobs in my country.
+                I am a curious frontend developer with two years of experience
+                in technology and user interface design. With a solid grasp of
+                frontend fundamentals, I am currently learning the popular React
+                library. Programming is a fascinating world that I become more
+                passionate about every day, and I am always ready to apply my
+                skills through continuous learning and experience.
             </p>
 
             <p className="hover:scale-105 transition-transform mb-5">
-                After immigrating to Europe, I decided to focus on another type
-                of creativity that has a common universal language, that is
-                technology world with the language of programming. In the last
-                few months, I have been diligently learning the fundamentals of
-                front end development. So far I've got a handle on HTML, CSS,
-                and JavaScript, and now I'm diving into React and its vast
-                ecosystem.
+                During this time, I have worked on numerous small and
+                medium-sized projects using React and TypeScript, gaining
+                valuable experience. I have always focused on creating
+                responsive and user-friendly interfaces across various devices
+                and browsers. Currently, I am learning Next.js to master new
+                techniques and tools that will help me tackle more complex web
+                development projects.
             </p>
             <p className="hover:scale-105 transition-transform">
-                It's hard to put into words how much I have been under the
-                influence of the indescribable realm of programming. Every day
-                I'm filled with learning the amazing capabilities of this
-                interesting and stunning domain.
+                With skills in responsive design, Atomic Design, and close
+                collaboration with teams, I am eager to apply my expertise to
+                new projects. For me, programming is not just a job, but an
+                endless world of learning and creativity that I grow more
+                passionate about every day.
             </p>
         </div>
     );
